bec6fe58a7d8fa7a2481a1922ffa25cd5883ed5a
mac <awais.ahmadfcc@gmail.com>
2020-05-09 12:07:39 +0500

    basic

--- app/src/main/java/com/example/filesynchor/Extras/Splash.java
+++ app/src/main/java/com/example/filesynchor/Extras/Splash.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.filesynchor.R;
 import com.example.filesynchor.SyncFileUtility;
 
